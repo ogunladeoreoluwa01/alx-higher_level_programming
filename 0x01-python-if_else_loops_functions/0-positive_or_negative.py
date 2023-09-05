7d3339bd5ed1943a8e87690612198d5e8b8a0268
dreamer-was-taken <emaore0707@gmail.com>
2023-09-05 19:46:29 +0100

    Update 0-positive_or_negative.py

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
+"""
+This script generates a random signed number and prints whether it's positive, negative, or zero.
+"""
+
 import random
+
+# Generate a random number between -10 and 10
 number = random.randint(-10, 10)
+
+# Check if the number is positive
 if number > 0:
+    # Print a message for a positive number
     print(f"{number} is positive")
 elif number < 0:
+    # Print a message for a negative number
     print(f"{number} is negative")
 else:
+    # Print a message for zero
     print(f"{number} is zero")
